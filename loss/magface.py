23e4c44a9289fb936dd8b33a52f38d35719d759e
Cheolhui-j <zkwmdi3626@gmail.com>
2022-05-01 23:16:21 +0900

    fix easy_margin & add original magface in magface

--- loss/magface.py
+++ loss/magface.py
@@ -20,7 +20,7 @@ class magface(nn.Module):
         self.u_m = 0.8
         self.l_m = 0.4
 
-        self.easy_margin = True
+        self.easy_margin = False
 
         # weight matrix init
         self.weight_matrix = torch.nn.Parameter(torch.empty(self.num_cls, self.emb_size))
@@ -28,22 +28,26 @@ class magface(nn.Module):
 
         # self.criterion = nn.CrossEntropyLoss()
 
-    def _calc_lambda(self):
-        lambda_g = ((self.scale*(self.u_a**2)*(self.l_a**2)) / ((self.u_a**2)-(self.l_a**2))) * (self.u_m-self.l_m)/(self.u_a - self.l_a)
-        return lambda_g
+    def _calc_m(self, embedding_norm):
+        m_a = (self.u_m-self.l_m)/(self.u_a - self.l_a) * (embedding_norm - self.l_a) + self.l_m 
+        return m_a
 
     def _calc_g(self, embedding_norm):
         g_a = 1 / embedding_norm + 1 / (self.u_a ** 2) * embedding_norm
         return torch.mean(g_a)
 
-    def forward(self, embedding, label):
+    def _calc_lambda(self):
+        lambda_g = ((self.scale*(self.u_a**2)*(self.l_a**2)) / ((self.u_a**2)-(self.l_a**2))) * (self.u_m-self.l_m)/(self.u_a - self.l_a)
+        return lambda_g
+
+    def forward_old(self, embedding, label):
 
         # 1. x, w normalize & 2. calculate x * w = similarity
         sim = nn.functional.linear(nn.functional.normalize(embedding ,p=2, dim=1, eps=1e-12), nn.functional.normalize(self.weight_matrix,p=2, dim=1, eps=1e-12))
 
         # only magface
         embedding_norm = torch.norm(embedding, dim=1, keepdim=True)
-        m_a = (self.u_m-self.l_m)/(self.u_a - self.l_a) * (embedding_norm - self.l_a) + self.l_m 
+        m_a = self._calc_m(embedding_norm)
         g_a = self._calc_g(embedding_norm)
         lambda_g = self._calc_lambda() 
 
@@ -62,23 +66,61 @@ class magface(nn.Module):
         index = F.one_hot(label, num_classes = self.num_cls).type(dtype = torch.bool)
 
         # 7. sim 
-        # sim = sim + index * (cos_add_m - cos)
+        # # add easy_margin 
         if self.easy_margin:
-            final_sim = torch.where(
+            cos_add_m = torch.where(
                 sim > 0, cos_add_m, theta)
         else:
-            th = math.cos(math.pi - m_a)
-            sinmm = math.sin(math.pi - m_a) * m_a
-            final_sim = torch.where(
+            th = torch.cos(torch.tensor(math.pi) - m_a)
+            sinmm = torch.sin(torch.tensor(math.pi) - m_a) * m_a
+            cos_add_m = torch.where(
                 sim > th, cos_add_m, theta - sinmm)
 
+        sim = sim + index * (cos_add_m - cos)
+
         # 8. s * sim
-        final_sim = final_sim * self.scale
+        sim = sim * self.scale
 
         # loss = self.criterion(sim, label)
         loss = F.cross_entropy(sim, label, reduction='none')
 
         return loss.mean() + lambda_g * g_a
+
+    def forward(self, embedding, label):
+
+        # 1. x, w normalize & 2. calculate x * w = similarity
+        sim = nn.functional.linear(nn.functional.normalize(embedding ,p=2, dim=1, eps=1e-12), nn.functional.normalize(self.weight_matrix,p=2, dim=1, eps=1e-12))
+
+        # only magface
+        embedding_norm = torch.norm(embedding, dim=1, keepdim=True)
+        m_a = self._calc_m(embedding_norm)
+        cos_m, sin_m = torch.cos(m_a), torch.sin(m_a)
+
+        g_a = self._calc_g(embedding_norm)
+        lambda_g = self._calc_lambda() 
+
+        cos = sim 
+        sin = torch.sqrt(1.0 - torch.pow(sim, 2))
+        cos_m = cos * cos_m - sin * sin_m
+
+        if self.easy_margin:
+            cos_m = torch.where(
+                cos > 0, cos_m, cos)
+        else:
+            th = torch.cos(torch.tensor(math.pi) - m_a)
+            sinmm = torch.sin(torch.tensor(math.pi) - m_a) * m_a
+            cos_m = torch.where(
+                cos > th, cos_m, cos - sinmm)
+
+        cos, cos_m = cos * self.scale, cos_m * self.scale
+
+        one_hot = torch.zeros_like(cos)
+        one_hot.scatter_(1, label.view(-1, 1), 1.0)
+        output = one_hot * cos_m + (1.0 - one_hot) * cos
+        loss = F.cross_entropy(output, label, reduction='mean')
+
+        return loss.mean() + lambda_g * g_a
+        
 
 if __name__ == "__main__":
 
@@ -86,7 +128,8 @@ if __name__ == "__main__":
     label = torch.randint(high=9, size=(64,))
 
     loss = magface()
+    loss2 = magface()
 
     print(loss(emb, label))
-    
+    print(loss2.forward_old(emb, label))
 
